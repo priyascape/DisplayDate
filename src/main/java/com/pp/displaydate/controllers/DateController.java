f93ab6913689274cf147087d8f1c3c1b1f5a1eb1
priyascape <priyascape@gmail.com>
2021-04-15 23:54:22 +0100

    DisplayDate

---
+++ src/main/java/com/pp/displaydate/controllers/DateController.java
@@ -0,0 +1,22 @@
+package com.pp.displaydate.controllers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class DateController {
+	@RequestMapping ("/date")
+	public String date(Model model) {
+		String datePattern = "EEEEE, dd MMMMM, yyyy";
+		SimpleDateFormat dateFormat = new SimpleDateFormat(datePattern);
+		String date = dateFormat.format(new Date());	
+		model.addAttribute("date", date);
+		return "/DisplayDate/date.jsp";
+
+	}
+}
